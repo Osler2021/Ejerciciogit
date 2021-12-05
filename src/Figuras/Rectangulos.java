7b54435bb530f5d5d63283c0011363b04f45772e
German <gmonti@agdcorp.com.ar>
2021-12-05 14:34:38 -0300

    Se agrega consturctor y atributos

--- src/Figuras/Rectangulos.java
+++ src/Figuras/Rectangulos.java
@@ -2,4 +2,11 @@ package Figuras;
 
 public class Rectangulos {
 
+	private double ancho;
+	private double alto;
+
+	public Rectangulos(double ancho, double alto) {
+		this.ancho = ancho;
+		this.alto = alto;
+	}
 }
